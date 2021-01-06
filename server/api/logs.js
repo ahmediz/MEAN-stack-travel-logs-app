c35d5e62825118f0ef206f427caac972982344bd
Ahmed Shaarawy <ahmedshaarawy891@gmail.com>
2021-01-06 12:01:09 +0200

    fixing something

--- server/api/logs.js
+++ server/api/logs.js
@@ -4,7 +4,7 @@ const LogEntry = require('../models/LogEntry');
 const router = Router();
 
 
-router.get('/', async (req,res) => {
+router.get('/', async (req,res, next) => {
      try {
         const logs = await LogEntry.find();
         res.json(logs);
@@ -17,6 +17,7 @@ router.post('/', async (req,res,next) => {
     try {
         const log = new LogEntry(req.body);
         const result = await log.save();
+        console.log(req.body)
         res.send(result);
     } catch (error) {
         if(error.name === 'ValidationError') {
